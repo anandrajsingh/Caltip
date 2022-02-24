ced2ae15c8ea1f17f7e9d4aa360f7990cb5a35e0
anandrajsingh <ayushpratap253@gmail.com>
2022-02-24 16:46:56 +0530

    Updated ClickListener

--- app/src/main/java/android/example/caltip/MainActivity.java
+++ app/src/main/java/android/example/caltip/MainActivity.java
@@ -1,14 +1,20 @@
 package android.example.caltip;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     private EditText txtAmount;
     private EditText txtPeople;
@@ -38,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
 
         btnReset = (Button) findViewById(R.id.btnReset);
         btnCalculate = (Button) findViewById(R.id.btnCalculate);
+        btnCalculate.setEnabled(false);
 
         txtTipAmount = (TextView) findViewById(R.id.txtTipAmount);
         txtTotalToPay = (TextView) findViewById(R.id.txtTotalToPay);
@@ -48,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
         rdoGroupTips.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
-                if (checkedId == R.id.radioFifteen || checkedId == R.id.radioTwenty){
+                if (checkedId == R.id.radioFifteen || checkedId == R.id.radioTwenty) {
                     txtTipOther.setEnabled(false);
                     btnCalculate.setEnabled(txtAmount.getText().length() > 0 && txtPeople.getText().length() > 0);
                 }
@@ -57,11 +64,12 @@ public class MainActivity extends AppCompatActivity {
                     txtTipOther.requestFocus();
 
                     btnCalculate.setEnabled(txtAmount.getText().length() > 0 && txtPeople.getText().length() > 0
-                        && txtTipOther.getText().length() > 0);
+                            && txtTipOther.getText().length() > 0);
                 }
                 radioCheckedId = checkedId;
             }
         });
+
 
         txtPeople.setOnKeyListener(mKeyListener);
         txtAmount.setOnKeyListener(mKeyListener);
@@ -70,6 +78,95 @@ public class MainActivity extends AppCompatActivity {
         btnCalculate.setOnClickListener(mClickListener);
         btnReset.setOnClickListener(mClickListener);
 
+    }
 
+        OnKeyListener mKeyListener = new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                switch (view.getId()) {
+                    case R.id.txtAmount:
+                    case R.id.txtPeople:
+                        btnCalculate.setEnabled(txtAmount.getText().length() > 0 && txtPeople.getText().length() > 0);
+                        break;
+                    case R.id.txtTipOther:
+                        btnCalculate.setEnabled(txtAmount.getText().length() > 0
+                                            && txtPeople.getText().length() > 0
+                                            &&  txtTipOther.getText().length() > 0);
+                        break;
+                }
+                return false;
+            }
+        };
+
+        OnClickListener mClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(view.getId() == R.id.btnCalculate){
+                    calculate();
+                }else{
+                    reset();
+                }
+            }
+        };
+
+
+
+    private void reset(){
+        txtAmount.setText("");
+        txtPeople.setText("");
+        txtTipOther.setText("");
+        txtTipAmount.setText("");
+        txtTotalToPay.setText("");
+        txtTipPerPerson.setText("");
+        rdoGroupTips.clearCheck();
+        rdoGroupTips.check(R.id.radioFifteen);
+        rdoGroupTips.requestFocus();
+    }
+
+    private void calculate() {
+        Double billAmount = Double.parseDouble(txtAmount.getText().toString());
+        Double totalPeople = Double.parseDouble(txtPeople.getText().toString());
+        Double percentage = null;
+        boolean isError = false;
+
+        if(billAmount < 1.0){
+            showErrorAlert("Enter a valid Total Amount.", txtAmount.getId());
+            isError = true;
+        }
+        if(radioCheckedId == -1){
+            radioCheckedId = rdoGroupTips.getCheckedRadioButtonId();
+        }
+        if(radioCheckedId == R.id.radioFifteen){
+            percentage = 15.00;
+        }
+        else if(radioCheckedId == R.id.radioTwenty){
+            percentage = 20.00;
+        }
+        else if(radioCheckedId == R.id.radioOther){
+            percentage = Double.parseDouble(txtTipOther.getText().toString());
+            if(percentage < 1.0){
+                showErrorAlert("Enter a valid Tip Percentage.", txtTipOther.getId());
+                isError = true;
+            }
+        }
+
+        if(!isError){
+            Double tipAmount = (billAmount * percentage)/100;
+            Double totalToPay = billAmount + tipAmount;
+            Double perPersonPays = totalToPay / totalPeople;
+
+            txtTipAmount.setText(tipAmount.toString());
+            txtTotalToPay.setText(totalToPay.toString());
+            txtTipPerPerson.setText(perPersonPays.toString());
+        }
+    }
+
+    private void showErrorAlert(String errorMessage, final int fieldId){
+        new AlertDialog.Builder(this).setTitle("Error").setMessage(errorMessage).setNeutralButton("Close", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                findViewById(fieldId).requestFocus();
+            }
+        }).show();
     }
 }
